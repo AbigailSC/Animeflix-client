20aaed9414bdcf3eb6c0ffc9b5e5134fdf89bd02
AbigailSc <abigailsarzuri@gmail.com>
2022-09-07 11:56:48 -0300

    refactor: cambiando configuracion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "standard-with-typescript"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
