9df390cf43d562ac35d53ade4cdc12ae613b3200
AbigailSc <abigailsarzuri@gmail.com>
2022-09-07 11:55:33 -0300

    refactor: cambiando configuracion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'plugin:react/jsx-runtime',
-    'eslint-config-prettier'
-  ],
+  extends: ["plugin:react/recommended", "standard-with-typescript"],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
   },
-  plugins: ['react'],
-  rules: {}
-}
+  plugins: ["react"],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
